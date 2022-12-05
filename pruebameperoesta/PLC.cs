4b2abc73ee9d131c18bf27ec2d0216337f9c4618
Jech <jechram5588@gmail.com>
2022-12-05 10:09:40 -0600

    Add project files.

---
+++ pruebameperoesta/PLC.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualBasic;
+using Corping.PLCConnector;
+using log4net;
+using System.Threading;
+
+
+namespace pruebameperoesta
+{
+    public class ConnectionPLC
+    {
+        private MemoryItem memRead;
+        private MemoryItem memWrite;
+        private MemoryItem memLevel;
+        private ILog Logger = LogManager.GetLogger(typeof(ConnectionPLC));
+
+        public ConnectionPLC(int otro, int nivel,int temp)
+        {
+            memRead = new MemoryItem();
+            memRead.Device = DeviceType.D;
+            memRead.Type = DataType.Int;
+            memRead.StartAddr = otro;
+            memRead.Size = 1;
+
+            memLevel = new MemoryItem();
+            memLevel.Device = DeviceType.D;
+            memLevel.Type = DataType.Int;
+            memLevel.StartAddr = nivel;
+            memLevel.Size = 1;
+
+            memWrite = new MemoryItem();
+            memWrite.Device = DeviceType.D;
+            memWrite.Type = DataType.Int;
+            memWrite.StartAddr = temp;
+        }
+
+        public object[] connected(PLC plc, int val)
+        {
+            try
+            {
+                object[] a = new object[2];
+                plc.WriteMemory(memWrite, val);
+                Thread.Sleep(200);
+                object obj = plc.ReadMemory(memRead);
+                if (obj != null)
+                {
+                    if (Information.IsNumeric(obj))
+                        a[0] = obj;
+                    else
+                        a[0] = 0;
+                }
+                else
+                    a[0] = 0;
+                Thread.Sleep(200);
+                object obj2 = plc.ReadMemory(memLevel);
+                if (obj2 != null)
+                {
+                    if (Information.IsNumeric(obj2))
+                        a[1] = obj2;
+                    else
+                        a[1] = 0;
+                }
+                else
+                    a[1] = 0;
+                return a;
+            }
+            catch (Exception ex)
+            {
+                Logger.ErrorFormat("Error plc:{0}", ex.Message);
+                return new object[] { 0, 0 };
+            }
+        }
+    }
+}
---
+++ pruebameperoesta/Program.cs
@@ -0,0 +1,57 @@
+﻿using log4net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace pruebameperoesta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var hilo = new Thread(() =>
+            {
+                ClassPrin conn = new ClassPrin();
+                conn.inicio();
+            });
+            
+        }
+    }
+
+    public class ClassPrin {
+
+        ILog Logger = LogManager.GetLogger("tst");
+
+        public ClassPrin() { 
+        }
+
+        public void inicio() {
+            Corping.PLCConnector.PLC _plc = new Corping.PLCConnector.PLC("192.168.5.200", 5000, true, 15000);
+            for (int i = 0; i < 1; i++)
+            {
+                var task = Task.Factory.StartNew(() => {
+                    lectura(_plc);
+                });
+            }
+           
+        }
+
+        public void lectura(Corping.PLCConnector.PLC _plc) {
+            int tempLeido = 200;
+            ConnectionPLC cplc = new ConnectionPLC(200, 10, 20);
+            while (true)
+            {
+                object lec = cplc.connected(_plc, tempLeido);
+                Console.WriteLine(lec);
+                //Logger.InfoFormat("Data Leido {0}",lec);
+                Thread.Sleep(5000);
+            }
+
+
+        }
+
+    }
+}
